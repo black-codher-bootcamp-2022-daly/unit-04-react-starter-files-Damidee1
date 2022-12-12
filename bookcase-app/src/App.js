3e78948325c3bcc91283343c84f5d857c26245f7
Damilola <opemipoolagunju@gmail.com>
2022-12-12 19:13:23 +0000

    add: class task

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
-import Book from './components/Book';
-import data from './models/books.json';
+import React, { useState } from "react";
+
+import { Book, PimpedBook } from "./components/Book";
+import data from "./models/books.json";
+import Search from "./components/Search";
 
 function App() {
-  const books = data;
-  return <Book/>;
+  // const books = data
+  const [books] = useState(data);
+  const favoBook = books[4];
+
+  function addBook(id) {
+    console.log("click happened to id: " + id);
+  }
+
+  return (
+    <Booklist>
+      <search></search>
+      <PimpedBook>
+        <h2>{favoBook.volumeInfo.title}</h2>
+        <p>{favoBook.volumeInfo.description}</p>
+      </PimpedBook>
+
+      <Book
+        handleClick={addBook}
+        id={favoBook.id}
+        title={favoBook.volumeInfo.title}
+        book={favoBook}
+      />
+
+      {books.map((item) => (
+        <PimpedBook>
+          <h2>{item.volumeInfo.title}</h2>
+          <p>{item.volumeInfo.description}</p>
+        </PimpedBook>
+      ))}
+    </Booklist>
+  );
+}
+
+function Booklist(props) {
+  return (
+    <div>
+      <h1>My library</h1>
+      <div className="booklist-container">{props.children}</div>
+    </div>
+  );
 }
 
 export default App;
-
---
+++ build-a-car/src/components/Skateboard.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Skateboard() {
+  return <h2>Hi, I am a Skateboard!</h2>;
+}
